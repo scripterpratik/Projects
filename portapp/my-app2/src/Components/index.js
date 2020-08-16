4450a45344a7764231080b9b9ab395c43425c74e
Pratik Chakraborty <pratikchakraborty007@gmail.com>
2020-08-16 21:59:07 +0530

    Added Navbar and Index

--- portapp/my-app2/src/Components/index.js
+++ portapp/my-app2/src/Components/index.js
@@ -1,11 +1,61 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Header from "./Header";
+import Particles from "react-particles-js";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  particle: { position: "absolute" },
+});
 
 const Home = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Header />
+      <Particles
+        canvasClassName={classes.particle}
+        params={{
+          particles: {
+            number: {
+              value: 70,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 1,
+                color: "tomato",
+              },
+            },
+            size: {
+              value: 8,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 8,
+                size_min: 0.1,
+                sync: true,
+              },
+            },
+            opacity: {
+              value: 1,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: true,
+              },
+            },
+          },
+        }}
+      />
+    </>
   );
 };
 
